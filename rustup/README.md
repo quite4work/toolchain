a9c71c8051dae95516d5e5ae2e424c3636c16293
tyranron <tyranron@gmail.com>
2019-09-27 11:16:14 +0200

    [rustup/nightly.sh] Add nightly rustup toolchain switching script

---
+++ rustup/README.md
@@ -0,0 +1,11 @@
+`rustup` related tools and snippets
+===================================
+
+- [`nightly.sh`](nightly.sh) allows to switch between desired `nightly` [Rust] toolchains freely via [`rustup`]. It installs `nightly-YYYY-MM-DD` [Rust] toolchain (`RUSTUP_NIGHTLY_DATE` env var is mandatory) and links it as default `nightly` [Rust] toolchain.
+
+
+
+
+
+[`rustup`]: https://rustup.rs
+[Rust]: https://www.rust-lang.org
---
+++ rustup/nightly.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 Instrumentisto Team
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+set -e
+
+
+if [[ -z "$RUSTUP_NIGHTLY_DATE" ]]; then
+  echo 'Error: RUSTUP_NIGHTLY_DATE must be specified'
+  exit 1
+fi
+RUSTUP_HOME=${RUSTUP_HOME:-~/.rustup}
+RUSTUP_DEFAULT_HOST_TRIPLE=$(cat $RUSTUP_HOME/settings.toml \
+                             | grep default_host_triple \
+                             | cut -d'"' -f2 \
+                             | tr -d '\n')
+
+
+set -x
+
+rustup install nightly-$RUSTUP_NIGHTLY_DATE
+ln -snf ~/.rustup/toolchains/nightly-$RUSTUP_NIGHTLY_DATE-$RUSTUP_DEFAULT_HOST_TRIPLE \
+       ~/.rustup/toolchains/nightly-$RUSTUP_DEFAULT_HOST_TRIPLE
+ln -snf ~/.rustup/update-hashes/nightly-$RUSTUP_NIGHTLY_DATE-$RUSTUP_DEFAULT_HOST_TRIPLE \
+       ~/.rustup/update-hashes/nightly-$RUSTUP_DEFAULT_HOST_TRIPLE
